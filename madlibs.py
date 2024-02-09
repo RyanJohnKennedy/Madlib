09b357619952bf1126faf5f32f63f1e879327545
RyanJohnKennedy <Ryan.Kennedy.John@gmail.com>
2024-02-09 14:38:20 +0200

    Initial

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to _____"
+youtuber = "Jeff Jeffinton"
+
+# a few way to do this
+print("Subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")
+
